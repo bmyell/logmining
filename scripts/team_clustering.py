de569e0f84552fe1c45f40e4ebce61bb8a04d97c
henriojord <henriojord@eisti.eu>
2015-06-01 23:25:20 +0900

    adding help for team_clustering

--- scripts/team_clustering.py
+++ scripts/team_clustering.py
@@ -8,7 +8,14 @@ import file_manager as fm
 
 if (len(sys.argv) == 3):
     print "Incoming..."
+    #Get all match
+    #For each match
+        #Give a label to t1 and to t2
+        #Get kick t1 et t2
+        #EMD
+        #Add value in distance matrix
+    #Print dendogram
+
 else:
     print "Argv error. Please type:"
-    print "python ball_trajectory.py log_path save_path/"
-    print "or python ball_trajectory.py log.rcg log.rcl save_path/"
+    print "python team_clustering.py log_path/ save_path/"
