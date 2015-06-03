04953f7edb2d2e6b5ddb7b45c1ee95700854f29a
henriojord <henriojord@eisti.eu>
2015-06-03 23:07:12 +0900

    now print success kick

--- scripts/count_corner_kick.py
+++ scripts/count_corner_kick.py
@@ -22,8 +22,8 @@ if (len(sys.argv) == 2):
         kick_data = fm.get_kick_data(rcl, ball_data)
         corner_kicks_chains = fm.get_corner_kicks_chains(kick_data, corner_kicks)
         success_corner_kicks = fm.get_success_corner_kicks(corner_kicks_chains)
-        #dv.plot_kick_chains(success_corner_kicks, teams, "")
-        
+        dv.plot_kick_chains(success_corner_kicks, teams, "")
+
         team1_corner_kicks = []
         team2_corner_kicks = []
         team1_success_corner_kicks = []
@@ -43,9 +43,11 @@ if (len(sys.argv) == 2):
 
         print str(file[0]) + ": "
         print "team: " + teams[0] + " " + str(len(team1_corner_kicks)) +\
-              " corner kicks"
+              " corner kicks " + str(len(team1_success_corner_kicks)) +\
+              " success"
         print "team: " + teams[1] + " " + str(len(team2_corner_kicks)) +\
-              " corner kicks"
+              " corner kicks " + str(len(team2_success_corner_kicks)) +\
+              " success"
 
         total_1 = total_1 + len(team1_corner_kicks)
         total_2 = total_2 + len(team2_corner_kicks)
@@ -53,12 +55,11 @@ if (len(sys.argv) == 2):
         total_s_2 = total_s_2 + len(team2_success_corner_kicks)
 
     print teams[0] + ": " + str(total_1) + " corner kicks " + "(" +\
-          str(len(team1_success_corner_kicks)) + ") "  "within " +\
+          str(total_s_1) + " success) "  "within " +\
           str(len(file_list)) + " matches."
     print teams[1] + ": " + str(total_2) + " corner kicks " + "(" +\
-          str(len(team2_success_corner_kicks)) + ") "  "within " +\
+          str(total_s_2) + " success) "  "within " +\
           str(len(file_list)) + " matches."
-
 else:
     print "Argv error. Please type:"
     print "python count_corner_kick.py log_path"
