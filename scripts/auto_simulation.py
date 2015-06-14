cde07974c921f3648e80cadc925f40761bfd8e44
henriojord <henriojord@eisti.eu>
2015-06-14 19:59:42 +0900

    adding NUM_SIM variable

--- scripts/auto_simulation.py
+++ scripts/auto_simulation.py
@@ -4,13 +4,13 @@ import time
 
 TEAM_L = "/home/scom/Documents/RoboCup/helios/helios/src/trainer.sh"
 OPP_LIST = ["/home/scom/Documents/RoboCup/team/rc_14/WrightEagle/start.sh",
-            "/home/scom/Documents/RoboCup/team/rc_14/Oxsy/start.sh",
-            
+            "/home/scom/Documents/RoboCup/team/rc_14/Oxsy/start.sh"
            ]
 NUM_OPP = len(OPP_LIST)
 LOG_DIRECTORY = "/home/scom/test/"
+NUM_SIM = 100
 
-for i in range(0, 100):
+for i in range(0, NUM_SIM):
     random_opp = random.randint(0, NUM_OPP - 1)
     TEAM_R = OPP_LIST[random_opp]
     p = subprocess.Popen([
