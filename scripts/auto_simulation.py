a3abe7bffe5d41268142349649e08f0cfe33ddfd
henriojord <henriojord@eisti.eu>
2015-06-16 17:02:01 +0900

    auto simulation: adding synch mode parameter

--- scripts/auto_simulation.py
+++ scripts/auto_simulation.py
@@ -12,7 +12,7 @@ OPP_LIST = ["/home/scom/Documents/RoboCup/team/rc_14/WrightEagle/start.sh",
            ]
 NUM_OPP = len(OPP_LIST)
 LOG_DIRECTORY = "/home/scom/test/"
-NUM_SIM = 100
+NUM_SIM = 5
 
 for i in range(0, NUM_SIM):
     random_opp = random.randint(0, NUM_OPP - 1)
@@ -20,9 +20,10 @@ for i in range(0, NUM_SIM):
     p = subprocess.Popen([
                     "rcssserver", \
                     "server::auto_mode=1", \
+                    "server::synch_mode=true", \
                     "server::team_l_start=" + TEAM_L, \
                     "server::team_r_start=" + TEAM_R, \
-                    "server::kick_off_wait=50", \
+                    "server::kick_off_wait=10", \
                     "server::game_logging=1", \
                     "server::text_logging=1", \
                     "server::game_log_dir=" + LOG_DIRECTORY, \
