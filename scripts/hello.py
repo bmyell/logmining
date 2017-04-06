e31f356b71b7ce023a705e9aa84b6004acde0306
bmyell <1812630796@qq.com>
2017-04-06 21:11:00 +0800

    first-done

--- scripts/hello.py
+++ scripts/hello.py
@@ -3,6 +3,6 @@
 #hello.py
 import re
 str="20170328215444-Q_learn_4-vs-HELIOS_base_2.rcg"
-patten=re.compile('\d{14}')
+patten=re.compile('\d{14}-[a-zA-Z]+\_[a-zA-Z]+\_[0-9]-vs-[a-zA-Z]+\_[a-zA-Z]+\_[0-9]')
 match=re.match(patten,str)
 print(match.group())
