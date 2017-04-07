0d6857fb91aca9a4294b14232d3cd02b86f0451a
bmyell <1812630796@qq.com>
2017-04-07 12:50:26 +0800

    first

--- scripts/hello.py
+++ scripts/hello.py
@@ -3,8 +3,6 @@
 #hello.py
 import re
 str="20170328215444-Q_learn_4-vs-HELIOS_base_2.rcg"
-# patten=re.compile('(\d{14}) ([a-zA-Z]+\_[a-zA-Z]+) ([0-9]) ([a-zA-Z]+)([a-zA-Z]+) ([0-9])')
-# match=re.search(patten,str)
 str2=str.split('-')
 time=str2[0]
 team_l=str2[1]
@@ -13,11 +11,8 @@ team_r=str2[3].strip('.rcg')
 team_patten=re.compile('[a-zA-Z]+\_[a-zA-Z]+')
 num_patten=re.compile('[0-9]+')
 
-
 t_l=re.search(team_patten,team_l).group()
 t_r=re.search(team_patten,team_r).group()
 score_l=re.search(num_patten,team_l).group()
 score_r=re.search(num_patten,team_r).group()
 
-
-print(time+' '+t_l+' '+score_l+' '+t_r+' '+score_r)
--- scripts/score_analysis.py
+++ scripts/score_analysis.py
@@ -1,30 +1,48 @@
 #!usr/bin/python2.7
 #-*-coding:utf-8-*
 
-import re #Regular Expression
-
+import re  # Regular Expression
 import os
 
+# need sort
 def get_file_in_dir(dir):
-    filelist =[]
-    filenames=os.listdir(dir)
+    filelist = []
+    filenames = os.listdir(dir)
     for fn in filenames:
         filelist.append(fn)
-    fo = open("list.txt",'wb')
+    fo = open("list.txt", 'wb')
     for j in filelist:
         fo.write(str(j))
         fo.write('\n')
-
-def get_infor_in_file_name(rclfile):
-    team_expr = "\d{14}-*-vs-*"
+    fo.close()
 
 
+def get_infor_of_on_line(line):
+    str2 = line.split('-')
+    time = str2[0]
+    team_l = str2[1]
+    team_r = str2[3].strip('.rcg')
 
-if __name__=="__main__":
-      dir='/home/rider/Desktop/Q_test/logs'
-      get_file_in_dir(dir)
+    team_patten = re.compile('[a-zA-Z]+\_[a-zA-Z]+')
+    num_patten = re.compile('[0-9]+')
+
+    t_l = re.search(team_patten, team_l).group()
+    t_r = re.search(team_patten, team_r).group()
+    score_l = re.search(num_patten, team_l).group()
+    score_r = re.search(num_patten, team_r).group()
+    print(time + " " + t_l + "   " + t_r + "   " + score_l + "   " + score_r)
 
 
+def handle_text():
+    f = open("list.txt", 'r')
+    line = f.readline()
+    while line:
+        get_infor_of_on_line(line)
+        line = f.readline()
+    f.close()
 
 
-
+if __name__ == "__main__":
+    dir = '/home/rider/Desktop/Q_test/logs'
+    get_file_in_dir(dir)  # works
+    handle_text()  # works
